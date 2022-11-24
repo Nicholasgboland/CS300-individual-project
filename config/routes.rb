54c94c8e1afc8b7175ddeb1744c24be085b2d0ad
Nicholas Boland <boland@example.com>
2022-11-24 03:53:52 +0000

    Add initial Devise stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "projects#index"
   resources :projects
 end
 
 Rails.application.routes.draw do
+  devise_for :users
   resources :projects
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
