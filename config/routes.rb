895924da8df94688132bd06c8e44f11c54073868
Nicholas Boland <boland@example.com>
2022-11-25 00:34:10 +0000

    Add Devise sign-in capabilities

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  root "projects#index"
+  devise_for :users
+  root to: "projects#index"
   resources :projects
 end
 
-Rails.application.routes.draw do
-  resources :projects
+#Rails.application.routes.draw do
+#  devise_for :users
+#  resources :projects
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
+#end
